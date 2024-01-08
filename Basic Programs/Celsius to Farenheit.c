1038f36710f87e28badd8112fd00476499d86d3a
Aishwarya Narayanan <133395050+Aishwarya-Narayanan03@users.noreply.github.com>
2024-01-08 12:56:52 +0530

    Delete Basic Programs directory

--- Basic Programs/Celsius to Farenheit.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	float celsius,farenheit;
-	printf("\n Enter temp in Celsius : ");
-	scanf("%f",&celsius);
-	farenheit = (1.8*celsius)+32;
-	printf("\nTemperature in Farenheit : %f",farenheit);
-}
